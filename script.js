72c2c3c6394fb3f4c4e5f33ca6d1390371c0dd3d
AbdulMoeedZahid <abdulmoeedzahid@usf.edu>
2025-08-01 18:34:49 -0500

    Basic Rock Paper Scissors run entirely in console. GUI missing.

--- script.js
+++ script.js
@@ -14,4 +14,32 @@ if (computerChoiceVariable < 0.34) {
 return computerChoice;
 }
 //console.log(computerChoiceVariable);
-console.log("Computer choice is: " + getComputerChoice());
+// console.log("Computer choice is: " + getComputerChoice());
+
+function getPlayerChoice() {
+let playerChoice = prompt("Please enter your choice: rock, paper, or scissors");
+playerChoice = playerChoice.toLowerCase();
+if (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors") {
+    alert("Invalid choice. Please try again.");
+    return getPlayerChoice();
+}
+return playerChoice;
+}
+
+// console.log("Player choice is: " + getPlayerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        return "You win! " + playerSelection + " beats " + computerSelection;
+    } else {
+        return "You lose! " + computerSelection + " beats " + playerSelection;
+    }
+}   
+
+console.log(playRound(getPlayerChoice(), getComputerChoice()));
